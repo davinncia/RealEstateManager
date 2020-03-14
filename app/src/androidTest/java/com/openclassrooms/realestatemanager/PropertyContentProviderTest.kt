11b984fc11a3d764aea71f8b4095012e46d794da
lucasmahe <mahelucas1@gmail.com>
2020-03-14 09:31:26 +0100

    Content Provider integration tests.

--- app/src/androidTest/java/com/openclassrooms/realestatemanager/PropertyContentProviderTest.kt
+++ app/src/androidTest/java/com/openclassrooms/realestatemanager/PropertyContentProviderTest.kt
@@ -2,12 +2,14 @@ package com.openclassrooms.realestatemanager
 
 import android.content.ContentResolver
 import android.content.ContentUris
-import android.content.ContentUris.withAppendedId
 import androidx.room.Room
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
 import com.openclassrooms.realestatemanager.db.PropertyRoomDatabase
+import com.openclassrooms.realestatemanager.model.Address
+import com.openclassrooms.realestatemanager.model.Property
 import com.openclassrooms.realestatemanager.provider.PropertyContentProvider
+import org.junit.After
 import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
@@ -16,45 +18,65 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4::class)
 class PropertyContentProviderTest {
 
-    //TODO: Content provider test
+    private lateinit var database: PropertyRoomDatabase
+    private lateinit var contentResolver: ContentResolver
 
-    // FOR DATA
-    private lateinit var mContentResolver: ContentResolver
 
     // DATA SET FOR TEST
-    private val propertyID: Long = 1
+    private val propertyID = 1
+    private val nonExistingPropertyId = -1
+    private val property = Property("HOUSE", 100000F, 30F, 2, "Cool flat.",
+            Address("NY", "street", 10), 0, "Phil")
+
 
     @Before
     fun setUp() {
-        Room.inMemoryDatabaseBuilder(InstrumentationRegistry.getInstrumentation().context,
+        //DB
+        database = Room.inMemoryDatabaseBuilder(InstrumentationRegistry.getInstrumentation().context,
                 PropertyRoomDatabase::class.java)
                 .allowMainThreadQueries()
                 .build()
-        mContentResolver = InstrumentationRegistry.getInstrumentation().context.contentResolver
+        //CONTENT RESOLVER
+        contentResolver = InstrumentationRegistry.getInstrumentation().context.contentResolver
     }
 
+    @After
+    fun closeDb() {
+        database.close()
+    }
+
+    //TODO ProviderTest: Run on test db ?
+/*
     @Test
-    fun getPropertyWhenNoPropertyInserted() {
+    fun getPropertyWhenNoPropertyInserted() { //It's actually going on real db, so not empty !
         //WHEN
-        val cursor = mContentResolver.query(withAppendedId(PropertyContentProvider.URI_PROPERTY, propertyID), null, null, null, null)
+        val cursor = contentResolver.query(withAppendedId(PropertyContentProvider.URI_PROPERTY, propertyID.toLong()), null, null, null, null)
         //THEN
         Assert.assertNotNull(cursor)
-        Assert.assertTrue(cursor!!.count == 1) //TODO: Why not 0 ??
+        Assert.assertTrue(cursor!!.count == 0)
         cursor.close()
     }
+ */
 
     @Test
-    fun getProperty() {
-        // GIVEN
-        //insert in db
-
+    fun whenFetchingNonExistingPropertyThenCursorIsEmpty() {
         //WHEN
-        val cursor = mContentResolver.query(ContentUris.withAppendedId(PropertyContentProvider.URI_PROPERTY, propertyID), null, null, null, null)
+        val cursor = contentResolver.query(ContentUris.withAppendedId(PropertyContentProvider.URI_PROPERTY, nonExistingPropertyId.toLong()), null, null, null, null)
         //THEN
         Assert.assertNotNull(cursor)
-        Assert.assertTrue(cursor!!.count == 1)
-        Assert.assertTrue(cursor.moveToFirst())
-        Assert.assertEquals(cursor.getString(cursor.getColumnIndexOrThrow("descriptionProperty")), "Cool")
+        Assert.assertTrue(cursor!!.count == 0)
+    }
+
+    @Test
+    fun getFirstPropertyOfTableIfExists() {
+        //WHEN
+        val cursor = contentResolver.query(ContentUris.withAppendedId(PropertyContentProvider.URI_PROPERTY, propertyID.toLong()), null, null, null, null)
+        //THEN
+        if (cursor!!.count == 1) {
+            //Make sure we're not running on an empty db
+            Assert.assertTrue(cursor.moveToFirst())
+            Assert.assertEquals(propertyID, cursor.getInt(cursor.getColumnIndexOrThrow("id")))
+        }
     }
 
 
